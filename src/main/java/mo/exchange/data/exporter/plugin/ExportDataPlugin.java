07f461bd0fb6f0136e851859d7f77830c4b6f1d3
alonzho <alonso.salinas.lopez@gmail.com>
2018-04-05 00:46:09 -0300

    Se agrega el modulo de importacion junto con el plugin de importacion de proyectos

---
+++ src/main/java/mo/exchange/data/exporter/plugin/ExportDataPlugin.java
@@ -0,0 +1,7 @@
+package mo.exchange.data.exporter.plugin;
+
+import mo.exchange.data.plugin.ExchangeableDataPlugin;
+
+public interface ExportDataPlugin extends ExchangeableDataPlugin {
+	
+}
---
+++ src/main/java/mo/exchange/data/exporter/plugin/ExportProjectPlugin.java
@@ -0,0 +1,7 @@
+package mo.exchange.data.exporter.plugin;
+
+import mo.exchange.data.plugin.ExchangeableProjectPlugin;
+
+public interface ExportProjectPlugin extends ExchangeableProjectPlugin {
+	
+}
