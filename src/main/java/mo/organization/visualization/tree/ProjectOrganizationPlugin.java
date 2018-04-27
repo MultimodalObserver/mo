de3e5b119fb226819b8d622bb75a08faf19e5eea
Gcurifuta <gustavo.curifuta@usach.cl>
2018-04-26 22:55:08 -0300

    Se corrige mal comportamiento de botón default tre

--- src/main/java/mo/organization/visualization/tree/ProjectOrganizationPlugin.java
+++ src/main/java/mo/organization/visualization/tree/ProjectOrganizationPlugin.java
@@ -1,12 +1,22 @@
 package mo.organization.visualization.tree;
 
+import bibliothek.util.xml.XElement;
+import bibliothek.util.xml.XIO;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import mo.core.I18n;
 import mo.core.plugin.Extends;
 import mo.core.plugin.Extension;
+import mo.core.ui.dockables.DockableElement;
 import mo.core.ui.dockables.DockablesRegistry;
 import mo.organization.OrganizationVisualizationMenuItemProvider;
 import mo.organization.ProjectOrganization;
@@ -53,6 +63,29 @@ public class ProjectOrganizationPlugin implements OrganizationVisualizationMenuI
                 DockablesRegistry.getInstance()
                         .addDockableInProjectGroup(projectFolder.getAbsolutePath(), dock);
 
+            }
+            
+            else{
+                
+                try {
+                    XElement root = XIO.readUTF(new FileInputStream(treeOrgFile));
+                    String projectName = root.getElement("name").getString();
+                    System.out.println(projectName+i18n.s("ProjectOrganizationPlugin.titleSufix"));
+                    DockableElement d = DockablesRegistry.getInstance().getDockableByTitle(projectName+i18n.s("ProjectOrganizationPlugin.titleSufix"));
+                    if(d!=null){
+                        d.setVisible(true);
+                        DockablesRegistry.DockableCheckBoxMenuItem menuItem = DockablesRegistry.getInstance().getDockableMeuItemByTitle(projectName+i18n.s("ProjectOrganizationPlugin.titleSufix"));
+                        if(menuItem!=null){menuItem.setSelected(true);}
+                    }
+                    
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(ProjectOrganizationPlugin.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(ProjectOrganizationPlugin.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            
+            
             }
         }
     }
